6171fdc2c942083e1dbfbf9b7fe47df2734c51e2
Prabu Weerasinghe <prabu84@gmail.com>
2020-03-28 20:05:55 +0000

    Convert `<Layout>` to a named export from a default export.
    Default exports suck.

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import Helmet from "react-helmet";
 import config from "../../../data/SiteConfig";
-import "./index.css";
 
-export default class MainLayout extends React.Component {
-  render() {
-    const { children } = this.props;
-    return (
-      <div className="layout-container">
-        <Helmet>
-          <meta name="description" content={config.siteDescription} />
-          <html lang="en" />
-        </Helmet>
-        {children}
-      </div>
-    );
-  }
-}
+export const Layout = ({ children }) => {
+  return (
+    <div>
+      <Helmet>
+        <meta name="description" content={config.siteDescription} />
+        <html lang="en" />
+      </Helmet>
+      {children}
+    </div>
+  );
+};
