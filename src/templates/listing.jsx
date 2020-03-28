c1ab7a75bfd8a2b58203005c348632cc76a4967c
Prabu Weerasinghe <prabu84@gmail.com>
2020-03-28 19:06:02 +0000

    Treat `<Layout>` just like any other `React` component.
    Simplify folder structure and absorb it into the components folder.

--- src/templates/listing.jsx
+++ src/templates/listing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Helmet from "react-helmet";
 import { graphql, Link } from "gatsby";
-import Layout from "../layout";
+import Layout from "../components/Layout";
 import PostListing from "../components/PostListing/PostListing";
 import SEO from "../components/SEO/SEO";
 import config from "../../data/SiteConfig";
