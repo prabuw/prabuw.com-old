a717f6a394d6d5b5bcd71badff1e206b174cc827
Prabu Weerasinghe <prabu84@gmail.com>
2020-04-12 16:54:07 +0100

    Only apply padding on the x-axis for `<Container>` for flexibility, since this is a column based site.

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const Container = ({ children }) => (
-  <div className="container mx-auto max-w-3xl p-8">{children}</div>
+  <div className="container mx-auto max-w-3xl px-8">{children}</div>
 );
